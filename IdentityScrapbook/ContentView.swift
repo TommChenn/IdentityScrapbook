29d096dc72f13bbdcb05d41d33024d0309a3ae8c
Tom Chen <115874655+TommyGGs@users.noreply.github.com>
2026-09-16 21:40:51 +0800

    Initial Commit

---
+++ IdentityScrapbook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IdentityScrapbook
+//
+//  Created by Tom  on 2026/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IdentityScrapbook/IdentityScrapbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdentityScrapbookApp.swift
+//  IdentityScrapbook
+//
+//  Created by Tom  on 2026/09/16.
+//
+
+import SwiftUI
+
+@main
+struct IdentityScrapbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
